c819b014bb0b967d001c5036306ace6f0e358616
spac3nerd <sorin.badila@yahoo.com>
2016-08-13 20:04:15 -0400

    Moving demo JS source to its own file

---
+++ demos/primitives/cube.js
@@ -0,0 +1,28 @@
+function initDemo() {
+	var canvas = document.getElementById("view");
+	var renderer = new quack.renderers.GLRenderer(canvas, {
+		width: canvas.clientWidth,
+		height: canvas.clientHeight,
+		clearColor: new quack.math.vector4(0.0, 0.0, 0.0, 1.0),
+		antialias: true
+	});
+	var scene = new quack.core.scene("scene1");
+	var cubeG = new quack.cubeGeometry(); //at (0,0,0)
+	var camera = new quack.camera.orthoCamera(-1.0, 1.0, 1.0, -1.0, 10, 0);
+	camera.setPosition(1, 2, 5);
+	camera.setLookAt(0, 0, 0);
+	camera.update();
+	scene.append(cubeG);
+
+	renderer.render(scene, camera);
+
+	var scaleX = document.getElementById("scaleX");
+	var scaleY = document.getElementById("scaleY");
+	var scaleZ = document.getElementById("scaleZ");
+	var button = document.getElementById("apply");
+	button.onclick = function(e) {
+		var valX = Number(scaleX.value) || 1, valY = Number(scaleY.value) || 1, valZ = Number(scaleZ.value) || 1;
+		cubeG.setScale(valX, valY, valZ);
+		renderer.render(scene, camera);
+	};
+}
